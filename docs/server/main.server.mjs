e69e9471def4ce1bbff45d8290718dc8dffe8eb6
jz <johnzx030@gmail.com>
2024-05-26 02:21:59 +0200

    feat

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-5GSVB6JC.mjs";import"./chunk-FEI4FODL.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-UR75FW6L.mjs";import"./chunk-FEI4FODL.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'first test for porfolio';
+  title = 'First test for porfolio';
 }
